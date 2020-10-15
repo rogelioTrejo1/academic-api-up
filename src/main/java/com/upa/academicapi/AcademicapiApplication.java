6912e8625dde2ce7bdfc5d46f965d5ed031c5000
Rogelio <jonex720@gmail.com>
2020-10-15 10:11:32 -0500

    init a proyect

---
+++ src/main/java/com/upa/academicapi/AcademicapiApplication.java
@@ -0,0 +1,12 @@
+package com.upa.academicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AcademicapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicapiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/upa/academicapi/AcademicapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.upa.academicapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcademicapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
