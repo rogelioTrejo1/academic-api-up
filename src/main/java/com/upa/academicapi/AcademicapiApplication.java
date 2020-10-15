ece4e1f728335248409e1b01b9fb51f395a9d641
Rogelio <jonex720@gmail.com>
2020-10-15 10:15:55 -0500

    Add a models

--- src/main/java/com/upa/academicapi/AcademicapiApplication.java
+++ src/main/java/com/upa/academicapi/AcademicapiApplication.java
@@ -3,7 +3,6 @@ package com.upa.academicapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class AcademicapiApplication {
 	public static void main(String[] args) {
